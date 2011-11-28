20da13787eddbf217a7791c184ac47fede35c50f
goker cebeci <dev@gokercebeci.com>
2011-11-28 06:28:20 +0200

    Uygulama v3.00 RC

--- themes/default/browsers/trident.js
+++
@@ -1,8 +0,0 @@
-document.createElement('header');
-document.createElement('hgroup');
-document.createElement('nav');
-document.createElement('menu');
-document.createElement('section');
-document.createElement('article');
-document.createElement('aside');
-document.createElement('footer');
